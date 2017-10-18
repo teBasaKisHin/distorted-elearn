6d8a3b84fabfc7c1fe60a237c781760f4ad7bab7
leader <leader931334@outlook.jp>
2017-10-18 16:55:08 +0900

    [add] runtime.txt requirements.txt

--- login.py
+++ login.py
@@ -1,4 +1,13 @@
 import requests as req
+import json
+
+with open("personalInfo.json", "r") as f:
+    data = json.load(f)
+
+with open("url.json", "r") as f:
+    url = json.load(f)
 
 ses = req.Session()
-res = ses.get()
+res = ses.post(url["login"], data=data)
+print(res.text)
+
