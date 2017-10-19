eb3a03d48c2a1816c275ac65ec8ffeb249d6c8d4
leader <leader931334@outlook.jp>
2017-10-19 16:13:56 +0900

    [add]inputData.json [remove]personalInfo.json url.json runtime.txt

--- login.py
+++ login.py
@@ -1,13 +1,15 @@
 import requests as req
 import json
 
-with open("personalInfo.json", "r") as f:
-    data = json.load(f)
+def main():
+    with open("inputData.json", "r") as f:
+        data = json.load(f)
 
-with open("url.json", "r") as f:
-    url = json.load(f)
+    ses = req.Session()
+    res = ses.post(data["url"]["login"], data=data["login"])
+    print(res.text)
 
-ses = req.Session()
-res = ses.post(url["login"], data=data)
-print(res.text)
+
+if __name__ == '__main__':
+    main()
 
