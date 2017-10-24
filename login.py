b78316f906b5d72eec67a9202390ef1014502818
leader <leader931334@outlook.jp>
2017-10-24 23:33:08 +0900

    [update]login.py

--- login.py
+++ login.py
@@ -1,14 +1,29 @@
+import sys
 import requests as req
 import json
+from bs4 import BeautifulSoup
+
 
 def main():
     with open("inputData.json", "r") as f:
         data = json.load(f)
-
+    
     ses = req.Session()
-    res = ses.post(data["url"]["login"], data=data["login"])
-    print(res.text)
+    res = ses.post(data["url"]["login"], headers=data["header"], data=data["login"])
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, "html.parser")
+    data["message"]["sesskey"]=soup.find("input", attrs={"name":"sesskey"}).get("value")
 
+    while True:
+        msg=str(input("Your message ('q'exits) :"))
+        if msg == "q":
+            break
+        data["message"]["message"]=msg
+        res = ses.post(data["url"]["message"], headers=data["header"], data=data["message"])
+        res.raise_for_status()
+        print("Message sent")
+
+    print("This app is finished")
 
 if __name__ == '__main__':
     main()
