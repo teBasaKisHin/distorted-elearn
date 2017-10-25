09cd89633f807d95861aa0c7650eb5fa2c94589c
leader <leader931334@outlook.jp>
2017-10-25 22:59:06 +0900

    [update] login.py

--- login.py
+++ login.py
@@ -7,22 +7,24 @@ def main():
     with open("inputData.json", "r") as f:
         data = json.load(f)
     
-    ses = req.Session()
-    res = ses.post(data["url"]["login"], headers=data["header"], data=data["login"])
-    res.raise_for_status()
-    soup = BeautifulSoup(res.text, "html.parser")
-    data["message"]["sesskey"]=soup.find("input", attrs={"name":"sesskey"}).get("value")
-
-    while True:
-        msg=str(input("Your message ('q'exits) :"))
-        if msg == "q":
-            break
-        data["message"]["message"]=msg
-        res = ses.post(data["url"]["message"], headers=data["header"], data=data["message"])
+    with req.Session() as ses:
+        res = ses.post(data["url"]["login"], headers=data["header"], data=data["login"])
         res.raise_for_status()
-        print("Message sent")
+        soup = BeautifulSoup(res.text, "html.parser")
+        data["session"]["sesskey"] = soup.find("input", attrs={"name":"sesskey"}).get("value")
+        data["message"].update(data["session"]) # or data["message"]["sesskey"] = data["session"]["sesskey"]
 
-    print("This app is finished")
+        while True:
+            msg=str(input("Your message ('q'exits) :"))
+            if msg == "q":
+                ses.get(data["url"]["logout"], headers=data["header"], params=data["session"])
+                print("this app is finished")
+                break
+            data["message"]["message"] = msg
+            res = ses.post(data["url"]["message"], headers=data["header"], data=data["message"])
+            res.raise_for_status()
+            print("Message sent")
+
 
 if __name__ == '__main__':
     main()
